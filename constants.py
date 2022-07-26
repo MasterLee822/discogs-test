f4e62ab0d5fdb45510a136a20f0b5db0ea0910c3
Christopher Lee <36520357+MasterLee822@users.noreply.github.com>
2022-07-26 11:17:52 -0700

    Update constants.py
    removed api key.  OOps

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 available_currencies = ['USD', 'GBP', 'EUR']
-currency_api_key = "V6m3vwqx7XXBcA7Mxfvfxnkc291IhIKgJwYrZcqe"
+currency_api_key = ""
 base_currency_api_url = f"https://api.currencyapi.com/v3/latest?apikey={currency_api_key}"
